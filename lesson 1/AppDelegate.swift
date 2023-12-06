2b278feea682a19a396a4dd1c46f59bd90d3d58c
kirill kurochkin <godoffirekirill@gmail.com>
2023-12-06 12:12:39 +0200

    xx-1. projectadded

---
+++ lesson 1/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  lesson 1
+//
+//  Created by Кирилл Курочкин on 04.09.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+
+}
+
---
+++ lesson 1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  lesson 1
+//
+//  Created by Кирилл Курочкин on 04.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        var isOk = 5
+         print(isOk)
+    }
+    //fdfd
+
+}
+
